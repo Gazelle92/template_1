f4e5f95379c0e692bac8b320bceb43154d90de21
jordan <hutswing12@gmail.com>
2026-03-23 18:29:48 +0900

    260323-3

--- src/app/components/common/Loading.tsx
+++ src/app/components/common/Loading.tsx
@@ -25,7 +25,7 @@ export default function Loading() {
   //const [isLoading, setIsLoading] = useState(true);
 
 
-  /*useEffect(() => {
+  useEffect(() => {
     const lenis = (window as any).lenis;
 
     if (lenis) {
@@ -34,7 +34,7 @@ export default function Loading() {
       window.scrollTo(0, 0);
     }
   }, [pathname, searchParams]);
-*/
+
   useEffect(() => {
 
     let cancelled = false;
